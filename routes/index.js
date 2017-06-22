8d8755d11937ec96cf6f483834e363ecdd6630e6
Cody Melton <codymelton@me.com>
2017-06-21 19:49:00 -0600

    Course routes success

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
-//  blog/routes/index.js
-
 // this index.js is basically a directory. Easier to have user types (for example)
 
 const express = require('express');
-
-
+const CourseRoutes = require('./courses/courses');
 
 module.exports = (app) => {
-//the calls below accept two arguments (route, the object from the articles.js routes)
-
+//the calls below accept two arguments (route, the object from the course.js routes)
+	app.post('/api/courses', CourseRoutes.createCourse);
+	app.get('/api/courses', CourseRoutes.getAll);
+	app.get('/api/courses/:course_id', CourseRoutes.getById);
+	app.put('/api/courses/:course_id', CourseRoutes.updateCourse);
+	app.delete('/api/courses/:course_id', CourseRoutes.removeCourse);
 }
