6c3c9afec52f78acb3335081b16da433b15856f6
Cody Melton <codymelton@me.com>
2017-06-22 20:53:32 -0600

    Signup page now functional.

--- client/src/components/SignUp/SignUp.js
+++ client/src/components/SignUp/SignUp.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import {Link} from 'react-router';
 
-const SignUp = () => (
-  <div>
-    <h1>Hello from SignUp.js component</h1>
-  </div>
+const SignUp = (props) => (
+ <div className="container">
+    <h3>New User Sign Up</h3>
+      <form>
+        <label>I am a...</label>
+        <select onChange={(event) => props.updateField('role', event.target.value)}>
+        	<option value="">Nothing</option>
+        	<option value="teacher">Teacher</option>
+        	<option value="subscriber">Subscriber</option>
+        </select> 
+
+        <label>First Name</label>
+        <input type='text' className="form-control" placeholder="first name"
+         onChange={(event) => props.updateField('firstName', event.target.value)}/>
+
+         <label>Last Name</label>
+        <input type='text' className="form-control" placeholder="last name"
+         onChange={(event) => props.updateField('lastName', event.target.value)}/>
+
+        <label>Email</label>
+        <input type='email' className="form-control" placeholder="email address"
+         onChange={(event) => props.updateField('email', event.target.value)}/>
+
+        <label>Password</label>
+        <input type='password' className="form-control" placeholder="password"
+         onChange={(event) => props.updateField('password', event.target.value)}/>
+
+        <button type="button" className="btn btn-success"
+          onClick={(event) => props.handleSubmit(event)}>Sign Up</button>
+        <Link className="btn btn-default" to={'/login'}>Returning User &gt; Login</Link>
+       </form>
+ </div>
 )
 
 export default SignUp;
--- client/src/containers/SignUp/SignUpContainer.js
+++ client/src/containers/SignUp/SignUpContainer.js
@@ -1,11 +1,45 @@
 import React, {Component} from 'react';
+import $ from 'jquery';
 import {SignUp} from '../../components';
+import {browserHistory} from 'react-router';
 
 class SignUpContainer extends Component {
+
+  state = {
+    firstName: undefined,
+    lastName: undefined,
+    email: undefined,
+    password: undefined,
+    role: undefined
+  }
+
+  updateField = this.updateField.bind(this)
+  updateField(field, value) {
+    const newState = {};
+    newState[field] = value;
+    this.setState(newState); //new state is an object that we just created
+   }
+
+	handleSubmit = this.handleSubmit.bind(this) //bind functions FIRST so that you don't forget
+	  handleSubmit(event){ //this is  function
+	    event.preventDefault();//this is a method (not a property of an object)
+	    console.log('STATE IS NOW: ', this.state);
+	    const local = {email: this.state.email, password: this.state.password};
+	    $.ajax({
+	      url: '/api/signup',
+	      method: 'POST',
+	      data: local
+	    }).done((response) => (
+	      console.log("SUCCESS AT SIGN UP")) ||
+	      (response._id) ? //users have to go back to the login page
+	      browserHistory.push('/login') :
+	      browserHistory.push(`/err/${response.message}`));
+	  }
+
   render(){
     return(
       <div>
-        <SignUp />
+        <SignUp updateField={this.updateField}  handleSubmit={this.handleSubmit} />
       </div>
     )
   }
