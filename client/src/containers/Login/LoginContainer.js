5ec9289bdd8c1cb370c56a746ad0d388fe726cf4
Cody Melton <codymelton@me.com>
2017-06-27 21:31:38 -0600

    Login code working

--- client/src/containers/Login/LoginContainer.js
+++ client/src/containers/Login/LoginContainer.js
@@ -1,14 +1,43 @@
 import React, {Component} from 'react';
+import {browserHistory} from 'react-router';
+import $ from 'jquery';
 import {Login} from '../../components';
 
-class LoginContainer extends Component {
+class LoginContainer extends Component{
+  state = {
+    email: undefined,
+    password: undefined
+  }
+  updateField = this.updateField.bind(this);
+  updateField(field, value){
+    const newState = {};
+    newState[field] = value;
+    this.setState(newState);
+  }
+  handleSubmit = this.handleSubmit.bind(this);
+  handleSubmit(event){
+    event.preventDefault();
+    console.log('state is now:', this.state);
+    const local = {
+      email: this.state.email,
+      password: this.state.password
+    }
+    $.ajax({
+      url: '/api/login',
+      method: 'POST',
+      data: local
+
+      //(response.user._id && response.user.role === "te") for if else statements tomorrow.
+    }).done((response) => (response.user._id) ? browserHistory.push('/SubscriberDashboard'):
+      browserHistory.push(`/error/${response.message}`));
+  }
   render(){
     return(
-      <div>
-        <Login />
-      </div>
+      <Login
+        updateField = {this.updateField}
+        handleSubmit = {this.handleSubmit}
+      />
     )
   }
 }
-
 export default LoginContainer;
