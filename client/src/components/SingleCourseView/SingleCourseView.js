c126ac1ba2dc6b715e22cbfaf5f93c328d4a804c
jenntress <jenntress@gmail.com>
2017-06-28 21:23:03 -0600

    Wednesday

--- client/src/components/SingleCourseView/SingleCourseView.js
+++ client/src/components/SingleCourseView/SingleCourseView.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router';
+import {coursecontainer} from './styles.css';
 
-
-const SingleCourseView = () => (
+const SingleCourseView = (props) => (
+  <div className={coursecontainer}>
   <div>
-    <h1>Hello from the SingleCourseView.js</h1>
+   <h2>{props.title}</h2>
+   <p>{props.description}</p>
+   <p>${props.price}</p>
+   <Link className="btn btn-success" to={`/CourseList/edit/${props.id}`}>Edit(not working yet)</Link>
+   <Link className="btn btn-default" to="/CourseList">Back to Courses</Link>
   </div>
-)
+
+  <div>
+    { props.lessons.length > 0 ? props.lessons.map((item, index) => (
+        <div key={index}>
+          <Link to={`/SingleLessonView/${item._id}`}>{item.title}(view not ready)</Link>
+        </div>
+      )): <p>No lessons for this course</p>
+    }
+  </div>
+</div>
+  )
+
 
 export default SingleCourseView;
--- client/src/containers/Login/LoginContainer.js
+++ client/src/containers/Login/LoginContainer.js
@@ -34,8 +34,8 @@ class LoginContainer extends Component{
   render(){
     return(
       <Login
-        updateField = {this.updateField}
-        handleSubmit = {this.handleSubmit}
+        updateField={this.updateField}
+        handleSubmit={this.handleSubmit}
       />
     )
   }
