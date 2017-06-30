13bd8e69b8ae9fc30c3a275e67ee59d2665f1ccc
Cody Melton <codymelton@me.com>
2017-06-29 20:38:52 -0600

    Single page view working

--- client/src/components/SingleLessonView/SingleLessonView.js
+++ client/src/components/SingleLessonView/SingleLessonView.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router';
+import { lessoncontainer } from './styles.css';
 
-
-const SingleLessonView = () => (
-  <div>
-    <h1>Hello from the SingleLessonView.js</h1>
+const SingleLessonView = (props) => (
+  <div className={lessoncontainer}>
+	  <div>
+		   <h2>{props.title}</h2>
+		   <p>Sequence: {props.sequence}</p>
+		   <p>{props.objective}</p>
+		   <p>{props.videoURL}</p>
+		   <p>Published: {props.published}</p>
+		   <p>Marked Complete: {props.markedComplete}</p>
+		   <p>Archived: {props.archived}</p>
+		   <Link className="btn btn-default" to="/CourseList">Back to Course(not working yet</Link>
+	  </div>
   </div>
 )
 
